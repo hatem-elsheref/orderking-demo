08998da810675ba43622bdbec781b5de3719f378
hatem-dev <hatem_mohamed_elsheref@yahoo.com>
2024-02-13 10:57:52 +0200

    add routes and services

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -17,7 +17,25 @@ class Product extends Model
         return $this->belongsTo(Category::class, 'category_id');
     }
 
-    public function imageScope() :string
+    public function searchScope($query) :BelongsTo
+    {
+        $keyword = request()->query('search');
+        return $query->when(!empty($keyword), function ($query) use ($keyword){
+            return $query->where('name', 'LIKE', "%$keyword%")
+                ->orWhere('description', 'LIKE', "%$keyword%")
+                ->orWhere('tags', 'LIKE', "%$keyword%");
+        });
+    }
+
+    public function searchFilter($query) :BelongsTo
+    {
+        $category = request()->query('category');
+        return $query->when(!empty($category), function ($query) use ($category){
+            return $query->whereIn('category_id', explode(',', $category));
+        });
+    }
+
+    public function getImageAttribute() :string
     {
         return '';
     }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::prefix('dashboard')->middleware('auth')->group(function (){
+   Route::resource('products'     , ProductController::class);
+   Route::resource('categories'   , CategoryController::class);
 });
+
+
+Route::view('/', 'welcome');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
